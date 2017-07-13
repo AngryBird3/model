51cebd204f64b99d2b17bebe6959716e0ad1c2e6
AngryBird3 <dhaaraa.darji@gmail.com>
2017-07-12 21:00:17 -0400

    Adding skeleton for decision tree

---
+++ HousingData/src/DecisionTree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+'''
+Created on July 12, 2017
+@author: Dhara
+'''
+class DecisionTree(object):
+    def __init__(self):
+        pass
+
+    def train(self, x, y):
+        """
+        @type x: ndarray (training data)
+        @type y: ndarray (training data label)
+        """
+        
--- HousingData/src/DecisionTreeOnHousingData.py
+++ HousingData/src/DecisionTreeOnHousingData.py
@@ -37,9 +37,20 @@ class BostonHousingData(object):
         return df.as_matrix()
 
     def read_data(self):
+        """
+        Reads data from tab separated text file into numpy array
+        """
         return self.read_data_from_data_frame(self.read_data_frame_from_text_file(self.train_path))
+
+    def normalize_data(self, data):
+        """
+        For each column/features do following
+        Find min(feature=f) and max(feaure=f)
+        (data[f] - min[f])/max[f]
+        """
+        return (A - np.amin(A, axis=0))/np.amax(A, axis=0)
 
 def main():
     boston_housing_data = BostonHousingData()
-    arr = boston_housing_data.read_data()    
+    arr = boston_housing_data.read_data()
 main()
